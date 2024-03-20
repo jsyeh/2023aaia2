395e094904710a5f6d4fc4f7199a16952291e1b4
Jeng-Sheng Yeh 葉正聖 <jsyeh@mail.mcu.edu.tw>
2024-03-20 12:41:18 +0800

    week05

---
+++ week05/week05-2b.cpp
@@ -0,0 +1,11 @@
+///從 week05-1.cpp 複製過來 week05-2b.cpp
+///Settings-Editor (左邊第7個)Source Formatter
+///選 K&R (C的爸爸)
+///CodeBlocks右鍵跳出視窗, Format use AStyle
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<3; i++) {
+        printf("i是%d ", i);
+    }
+}
---
+++ week05/week05-2d.cpp
@@ -0,0 +1,10 @@
+///從 week05-1.cpp 複製過來 week05-2d.cpp
+///Settings-Editor (左邊第7個)Source Formatter
+///選 Java
+///CodeBlocks右鍵跳出視窗, Format use AStyle
+#include <stdio.h>
+int main() {
+    for(int i=0; i<3; i++) {
+        printf("i是%d ", i);
+    }
+}
